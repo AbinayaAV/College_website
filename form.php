a9e236456da83484bf8c4b0ae5d0fb6b1e78ade1
AbinayaAV <159990479+AbinayaAV@users.noreply.github.com>
2024-04-25 15:51:15 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,18 @@
+<?php
+$name=$_POST['name'];
+$visitor_email=$_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+$email_from='the domainname..com';
+$email_subject='New form submission';
+$email_body="User name:$name.\n";
+            "User email:$visitor_email.\n";
+            "Subject:$subject.\n";
+            "User message:$message.\n";
+$to ='xyzdspa@gmail.com';
+$headers="From : $email_from\r\n";
+$headers.="Reply_to:$visitor_email\r\n;
+ mail($to,$email_subject,$email_body,$headers);
+ header("Location:contact.html");
+ ?>
+
